fe814b3c0b6c1802fe3c978a2439d0a8d75a3cbc
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-06-06 15:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ dodger.style.backgroundColor = '#FF69B4';
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left
+    let moveLeft = parseInt(leftNumbers)
   }
 });
